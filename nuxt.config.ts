885205093a4448ab40926d82f6151e79ff0f9ebe
Jbuck <joeybuck1237@gmail.com>
2025-03-20 18:54:46 -0400

    Updated app title in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: 'Do'
+    }
+  },
   vite: {
     plugins: [tailwindcss()],
   },
