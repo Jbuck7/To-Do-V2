a8ec5028904bbf99b46c4bae07690ec94927a058
Jbuck <joeybuck1237@gmail.com>
2025-03-20 18:46:20 -0400

    add navigation menu with points display

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <SharedNavMenu />
+    <main class="pt-4">
+      <NuxtPage />
+    </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import SharedNavMenu from '~/components/shared/nav-menu.vue';
+</script>
